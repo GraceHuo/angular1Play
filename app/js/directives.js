f43a1d2e413d5bbfb673142d23eee5030a83c6fe
Grace Huo <Grace.Huo@intersystems.com>
2017-11-15 09:17:03 -0500

    sample directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,2 +1,16 @@
 'use strict';
 
+eventsApp.directive( 'mySample', function( $compile ) {
+  // return {
+  //   link: function( scope, element, attrs, controller ) {
+  //     var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>";
+  //     angular.element( element ).html( $compile( markup )( scope ) );
+  //   }
+  // };
+
+  // if all we what is replace the directive with some html, we don't need to do the link function
+  return {
+      restrict: 'E',
+      template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
+  };
+} );
